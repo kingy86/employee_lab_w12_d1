7f368c45676e0c03f3a2dca7fd206d1aaa9265ad
Mark <nirvanafoo86@hotmail.com>
2019-05-13 18:58:49 +0100

    Completed test for DatabaseAdmin for getName & getNationalInsuranceNumber

---
+++ src/main/java/TechStaff/DatabaseAdmin.java
@@ -0,0 +1,12 @@
+package TechStaff;
+
+import Staff.Employee;
+
+public class DatabaseAdmin extends Employee {
+
+    public DatabaseAdmin(String name, int nationalInsurance, double salary){
+        super(name, nationalInsurance, salary);
+    }
+
+
+}
---
+++ src/test/java/DatabaseAdminTest.java
@@ -0,0 +1,29 @@
+import TechStaff.DatabaseAdmin;
+import TechStaff.Developer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before(){
+        databaseAdmin = new DatabaseAdmin("Jimmy Jones", 735421, 35000);
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("Jimmy Jones", databaseAdmin.getName());
+    }
+
+    @Test
+    public void getNationalInsuranceNumber(){
+        assertEquals(735421, databaseAdmin.getNationalInsurance());
+    }
+
+    
+
+}
